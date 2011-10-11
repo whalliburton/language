f077fa08d8882808c3bfa53426cf04616e35e5c8
William Halliburton <whalliburton@gmail.com>
2011-10-11 00:23:12 -0400

    allow for more than one line in quiz-keys

--- keyboard.lisp
+++ keyboard.lisp
@@ -3,21 +3,23 @@
 (defparameter *russian-characters*
   '("фывапролджэ"
     "йцукенгшщзхъ"
-    "ЯЧСМИТЬБЮ"
-    "ёйцукенгшщзхъфывапролджэячсмитьбю"))
+    "ячсмитьбю"))
 
 (defun quiz-keys (&key (line 0) (shuffle #'shuffle))
-  (let (remaining)
-    (iter
-     (when (null remaining)
-       (setf remaining (funcall (or shuffle #'identity)
-                                (coerce (nth line *russian-characters*) 'list))))
-     (for char = (pop remaining))
-     (format t "~A~%" char)
-     (let ((in (read-char-from-emacs)))
-       (if (char= in #\q)
-         (return)
-         (unless (char= in char)
-           (format t "WRONG! (~A)~%" in)))))))
-
-
+  (let ((line
+          (etypecase line
+            (number line)
+            (cons (with-output-to-string (stream)
+                    (iter (for index in line)
+                          (write-string (nth index *russian-characters*) stream)))))))
+    (let (remaining)
+      (iter
+       (when (null remaining)
+         (setf remaining (funcall (or shuffle #'identity) (coerce line 'list))))
+       (for char = (pop remaining))
+       (format t "~A~%" char)
+       (let ((in (read-char-from-emacs)))
+         (if (char= in #\q)
+           (return)
+           (unless (char= in char)
+             (format t "WRONG! (~A)~%" in))))))))
